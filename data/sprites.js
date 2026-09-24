f56cc6e9c1cc86e1f71b246841719307d4a7c45b
mylifereading6666-oss <mylife.reading6666@gmail.com>
2026-09-24 19:51:30 +0900

    Add Sprite Check Web core files

---
+++ data/sprites.js
@@ -0,0 +1,6 @@
+window.SPRITES=[
+{id:"sample-001",name:"サンプルSprite 001",season:"Sample",releaseDate:"",status:"released",imageUrl:"",isNew:false},
+{id:"sample-002",name:"サンプルSprite 002",season:"Sample",releaseDate:"",status:"released",imageUrl:"",isNew:false},
+{id:"sample-003",name:"サンプルSprite 003",season:"Sample",releaseDate:"",status:"upcoming",imageUrl:"",isNew:true},
+{id:"sample-004",name:"サンプルSprite 004",season:"Sample",releaseDate:"",status:"uncertain",imageUrl:"",isNew:false}
+];
